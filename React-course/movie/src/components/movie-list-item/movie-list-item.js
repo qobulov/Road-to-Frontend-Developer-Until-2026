55a36ebec23132c53b4883cfa2573c5b6ff820a6
Azizbek <azizbekqobulov05@gmail.com>
2025-10-29 16:50:54 +0500

    feat: Enhance MovieList and MovieListItem components to accept and display movie data dynamically

--- React-course/movie/src/components/movie-list-item/movie-list-item.js
+++ React-course/movie/src/components/movie-list-item/movie-list-item.js
@@ -1,25 +1,28 @@
+import { Component } from "react";
 import "./movie-list-item.css";
 
-const MovieListItem = ({ name, views, color }) => {
-  return (
-    <li className={`list-group-item d-flex ${color && 'colors'}`}>
-      <span className="movie-list-item-label">{name}</span>
-      <input
-        type="number"
-        className="movie-list-item-input"
-        defaultValue={views}
-      />
-      <div className="controls">
-        <button type="button" className="btn-cookie btn-sm">
-          <i className="fas fa-cookie" />
-        </button>
-        <button type="button" className="btn-trash btn-sm">
-          <i className="fas fa-trash" />
-        </button>
-        <i className="fas fa-star"></i>
-      </div>
-    </li>
-  );
-};
-
+class MovieListItem extends Component {
+  render() {
+    const { name, views, color } = this.props;
+    return (
+      <li className={`list-group-item d-flex ${color && "colors"}`}>
+        <span className="movie-list-item-label">{name}</span>
+        <input
+          type="number"
+          className="movie-list-item-input"
+          defaultValue={views}
+        />
+        <div className="controls">
+          <button type="button" className="btn-cookie btn-sm">
+            <i className="fas fa-cookie" />
+          </button>
+          <button type="button" className="btn-trash btn-sm">
+            <i className="fas fa-trash" />
+          </button>
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
+}
 export default MovieListItem;
